b4e79dc1b1f54e985406957f52a6f0fadd6b63c5
matanyaglik <matanya.g@gmail.com>
2019-01-07 20:30:12 +0200

    add index ejs

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var path = require("path");
 var bodyParser = require("body-parser");
 var express = require("express");
+var ejs = require("ejs");
 var app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.sendFile("./index.html");
+  res.render("../public/index.ejs");
 });
 
 app.listen(3000, function() {
-  console.log("Example app listening on port 8080!");
+  console.log("Example app listening on port 3000!");
 });
