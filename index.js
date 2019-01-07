90a77cffab6f2c3b793df8fb8328b9eac691aee4
matanyaglik <matanya.g@gmail.com>
2019-01-08 00:28:15 +0200

    added authentication and login modal, users.json

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var ejs = require("ejs");
 var app = express();
+let users = require("./public/users.json");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("../public/index.ejs");
+});
+
+app.post("/users", (req, res) => {
+  const password = req.body.password;
+  const username = req.body.username;
+  console.log("hiiiiii");
+  if (username in users && users[username].password === password) {
+    res.send().status(200);
+  } else {
+    res.status(500).send("Authentication Error");
+  }
 });
 
 app.listen(3000, function() {
